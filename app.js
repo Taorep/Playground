d427fb17d9bbc6027941389f902e225b290465b6
Reggie Peterson <tao.rep@gmail.com>
2019-07-19 08:18:58 -0400

    successful implementation

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
-
+//Imperitive
 // const getColorAvg = (...colors) => {
 //   let rgbColor = [],
 //     result = []
 //   colors.forEach(color => {
+//     console.log(color)
 //     color = color.split``
 //     for (val in color) {
+//       parse
 //       if (parseInt(val) % 2 === 0) {
 //         rgbColor.push(parseInt((color.slice(val, parseInt(val) + 2).join``).toString(), 16))
 //       }
@@ -18,14 +20,31 @@
 //   console.log(`#${result.join``.toUpperCase()}`)
 // }
 
-const toRGB = colors => colors.map(color => color.split``)
+//Functional?
 
-const getColorAvg = (...colors) => {
-  console.log(toRGB(colors))
+const hexToRGB = colors => {
+  return colors.map(color=> {
+    return [color.slice(0,2), color.slice(2,4),color.slice(4)].map(val=> {
+      parseInt(val,16)
+    })
+  })
 }
 
+const getAvgRGB = (color1,color2) => {
+  return color1.map((val,ind) => {
+    parseInt((val + color2[ind]) / 2)
+  })
+}
 
+const calcHexAvg = (...colors) => {
+  const newColor = getAvgRGB(...hexToRGB(colors))
+  return `#${newColor.map(val=> {
+    return val.toString(16)
+  }).join("").toUpperCase()}`
+}
 
+// const hexToRGB = colors => colors.map(color=> [color.slice(0,2), color.slice(2,4),color.slice(4)].map(val=>parseInt(val,16)))
+// const calcNewColor = (color1,color2) => color1.map((val,ind) => parseInt((val + color2[ind]) / 2))
+// const getAvgColor = (...colors) => `#${calcNewColor(...hexToRGB(colors)).map(val=> val.toString(16)).join("").toUpperCase()}`
 
-
-getColorAvg('a1b2c3', 'BADAE5')
+console.log(calcHexAvg('a1b2c3', 'BADA55'))
